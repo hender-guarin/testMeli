5eca235652d38631bcc985285734224ebfa35e4e
Hender_Guarin <henderguarin@wposs.com>
2022-07-27 10:22:58 -0500

    Se agrega proyecto base para iniciar con el modelo mvp para desarrollar la prueba

---
+++ app/src/main/java/com/testmeli/MVP/vista/ActivityBuscador.java
@@ -0,0 +1,16 @@
+package com.testmeli.MVP.vista;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.testmeli.R;
+
+public class ActivityBuscador extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
